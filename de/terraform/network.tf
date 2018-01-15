232110aa9062c28c942e8dfc3efe5494fe71bdf6
Lisa Hu <lhu@financialengines.com>
2018-01-14 23:04:51 -0800

    ANLY-2820: some renaming to make the network more general (continue). +review FINR-2

--- de/terraform/network.tf
+++ de/terraform/network.tf
@@ -4,40 +4,40 @@ provider "aws" {
 }
 
 # Select a VPC to launch our instances into
-data "aws_vpc" "finr_vpc" {
+data "aws_vpc" "vpc" {
   id = "${lookup(var.vpc_id, terraform.workspace)}"
 }
 
-output "finr_vpc" {
-  value = "${data.aws_vpc.finr_vpc.id}"
+output "vpc" {
+  value = "${data.aws_vpc.vpc.id}"
 }
 
-output "finr_vpc_cidr" {
-  value = "${data.aws_vpc.finr_vpc.cidr_block}"
+output "vpc_cidr" {
+  value = "${data.aws_vpc.vpc.cidr_block}"
 }
 
 #subnet - private 1
-data "aws_subnet" "finr_private1" {
+data "aws_subnet" "private1" {
   id = "${lookup(var.subnet_private1_id, terraform.workspace)}"
 }
 
-output "finr_subnet_private1_cidr" {
-  value = "${data.aws_subnet.finr_private1.cidr_block}"
+output "subnet_private1_cidr" {
+  value = "${data.aws_subnet.private1.cidr_block}"
 }
 
-output "finr_subnet_private1_az" {
-  value = "${data.aws_subnet.finr_private1.availability_zone}"
+output "subnet_private1_az" {
+  value = "${data.aws_subnet.private1.availability_zone}"
 }
 
 #subnet - private 2
-data "aws_subnet" "finr_private2" {
+data "aws_subnet" "private2" {
   id = "${lookup(var.subnet_private2_id, terraform.workspace)}"
 }
 
-output "finr_subnet_private2_cidr" {
-  value = "${data.aws_subnet.finr_private2.cidr_block}"
+output "subnet_private2_cidr" {
+  value = "${data.aws_subnet.private2.cidr_block}"
 }
 
-output "finr_subnet_private2_az" {
-  value = "${data.aws_subnet.finr_private2.availability_zone}"
+output "subnet_private2_az" {
+  value = "${data.aws_subnet.private2.availability_zone}"
 }
