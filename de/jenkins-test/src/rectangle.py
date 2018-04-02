e8fa5935812fcd0bdb66360bb8fdf2ec80255c44
Lisa Hu <lhu@financialengines.com>
2018-04-02 13:39:19 -0700

    ANLY-2977:create src

---
+++ de/jenkins-test/src/rectangle.py
@@ -0,0 +1,11 @@
+# Python Program to find Area of a Rectangle using Functions
+ 
+def getArea(width, height):
+ 
+    # calculate the area
+    return width * height
+
+def getPerimeter(width, height):
+ 
+    # calculate the Perimeter
+    return 2 * (width + height)
